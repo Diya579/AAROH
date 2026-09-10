52d7c400c77ad5c44fad156f89ad937912c368fa
Yashashvi2407 <mparekh2407@gmail.com>
2026-09-10 19:56:10 +0530

    Delete Frontend directory

--- Frontend/src/components/analytics/index.js
+++
@@ -1,6 +0,0 @@
-export { RiskDistributionChart } from './RiskDistributionChart';
-export { LongitudinalTrendChart } from './LongitudinalTrendChart';
-export { InterventionMetricsChart } from './InterventionMetricsChart';
-export { ResponseTimeChart } from './ResponseTimeChart';
-export { OutcomeDistributionChart } from './OutcomeDistributionChart';
-export { DistrictComparisonChart } from './DistrictComparisonChart';
